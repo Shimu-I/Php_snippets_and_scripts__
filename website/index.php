991feaeb7c8d18fd1269f088f1c3e803bd882060
Shimu-I <jasmin851t626@gmail.com>
2024-11-05 02:20:05 +0600

    Ongoing lesson 2 and the video till 00:20:24.

---
+++ website/index.php
@@ -0,0 +1,65 @@
+<?php
+// Lesson 1: Introduction to php
+//******************************/
+    /*
+    single line comment: //
+    multiline comment: /**
+    */ 
+
+    /*actual code
+        echo"I love watermelon <br>";
+        echo"It's really good <br>";
+    */
+
+
+
+// Lesson 2: Variable and data types
+//******************************/
+/*variable = a reusable container that holds data string, integer, float, boolean*/
+
+$name = "Violet Evergarden";
+$food = "watermelon";
+$email = "fake@gmail.com";
+$age = 21;
+$users = 2;
+$quantity = 3;
+
+
+echo $name;
+
+echo "Hello {$name}<br>";
+echo "I like {$food}<br>";
+echo "My mail address is: {$email}<br>";
+echo "You are {$age} years old<br>";
+echo "There are {$users} users online.<br>";
+echo "You would like to buy {$quantity} items<br>"
+
+//....20:24
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!-- 
+Lesson 1 part
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order a pizza</button>
+</body>
+</html>
+-->
